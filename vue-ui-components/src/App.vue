1e035e99591f300cb2da59f8fc3e37e8ae9a32bd
cadu pavao <cadupavao@gmail.com>
2023-02-16 14:46:38 -0300

    feat: init vue 3 + vite

---
+++ vue-ui-components/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Exhibition from "./view/Exhibition.vue";
+</script>
+
+<template>
+  <Exhibition />
+</template>
+
+<style scoped>
+body {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  background-color: rgb(26, 23, 39);
+}
+</style>
---
+++ vue-ui-components/src/view/Exhibition.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <h2>Hello! i'm a test page :)</h2>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  font-size: 4rem;
+  color: #ffffff;
+}
+</style>
